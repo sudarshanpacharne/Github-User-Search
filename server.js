3e736c74ec665c09d8d186758dc949444da7d841
aniruddha-coditas <aniruddha.gohad@coditas.com>
2019-01-25 12:05:01 +0530

    build dependencies

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/github-user-search'));
 
-app.get('/*', function(req,res) {
+//app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/github-user-search/index.html'));
-});
+//res.sendFile(path.join(__dirname,'/dist/github-user-search/index.html'));
+//});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log("listening on PORT 8080");
